827e9e70dd5848d52d189719308347b69bbd174d
Isaac Nemzer <ijnemzer@outlook.com>
2019-03-26 01:45:57 -0400

    Mapbox GL react demo

--- src/index.js
+++ src/index.js
@@ -1,12 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import mapboxgl from 'mapbox-gl';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import 'mapbox-gl/dist/mapbox-gl.css';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+mapboxgl.accessToken = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA';
+
+class Application extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      lng: 5,
+      lat: 34,
+      zoom: 1.5
+    };
+  }
+
+  componentDidMount() {
+    const { lng, lat, zoom } = this.state;
+
+    const map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: 'mapbox://styles/mapbox/dark-v10',
+      center: [lng, lat],
+      zoom
+    });
+
+    map.on('move', () => {
+      const { lng, lat } = map.getCenter();
+
+      this.setState({
+        lng: lng.toFixed(4),
+        lat: lat.toFixed(4),
+        zoom: map.getZoom().toFixed(2)
+      });
+    });
+  }
+
+  render() {
+    const { lng, lat, zoom } = this.state;
+
+    return (
+      <div>
+        <div className="inline-block absolute top left mt12 ml12 bg-darken75 color-white z1 py6 px12 round-full txt-s txt-bold">
+          <div>{`Longitude: ${lng} Latitude: ${lat} Zoom: ${zoom}`}</div>
+        </div>
+        <div ref={el => this.mapContainer = el} className="absolute top right left bottom" />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Application />, document.getElementById('root'));
