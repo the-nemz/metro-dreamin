ced21cd17a20f0a1eae0090f72cf96dc22219d66
Isaac Nemzer <ijnemzer@outlook.com>
2019-03-28 04:14:55 -0400

    Able to add stations and a line is drawn through them

--- src/index.js
+++ src/index.js
@@ -12,42 +12,82 @@ class Application extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      lng: 5,
-      lat: 34,
-      zoom: 1.5
+      stations: [],
+      zoom: 2
     };
   }
 
   componentDidMount() {
-    const { lng, lat, zoom } = this.state;
+    const { zoom } = this.state;
 
     const map = new mapboxgl.Map({
       container: this.mapContainer,
       style: 'mapbox://styles/mapbox/dark-v10',
-      center: [lng, lat],
-      zoom
+      zoom: zoom
     });
 
-    map.on('move', () => {
-      const { lng, lat } = map.getCenter();
+    map.on('click', (e) => {
+      const { lng, lat } = e.lngLat;
+
+      const stations = this.state.stations.slice(0, this.state.stations.length + 1);
 
       this.setState({
-        lng: lng.toFixed(4),
-        lat: lat.toFixed(4),
-        zoom: map.getZoom().toFixed(2)
+        stations: stations.concat([[lng, lat]])
       });
+      // console.log(this.state.stations);
+
+      const svgCircle = '<svg height="16" width="16"><circle cx="8" cy="8" r="6" stroke="#000" stroke-width="2" fill="#fff" /></svg>';
+
+      let el = document.createElement('button');
+      el.className = 'Map-station';
+      el.innerHTML = svgCircle;
+
+      new mapboxgl.Marker(el)
+        .setLngLat([lng, lat])
+        .addTo(map);
+
+      if (this.state.stations.length > 1) {
+        let newlayer = {
+          "id": "Map-line" + this.state.stations.length,
+          "type": "line",
+          "source": {
+            "type": "geojson",
+            "data": {
+              "type": "Feature",
+              "properties": {},
+              "geometry": {
+                "type": "LineString",
+                "coordinates": this.state.stations
+              }
+            },
+          },
+          "layout": {
+              "line-join": "round",
+              "line-cap": "round"
+          },
+          "paint": {
+              "line-color": "#ff0000",
+              "line-width": 8
+          }
+        };
+
+        map.addLayer(newlayer);
+
+        if (this.state.stations.length > 2) {
+          map.removeLayer(`Map-line${this.state.stations.length - 1}`);
+        }
+      }
     });
   }
 
   render() {
-    const { lng, lat, zoom } = this.state;
+    const { lng, lat } = this.state;
 
     return (
       <div className="Map">
         <div className="Map-textWrap">
-          <div className="Map-text">{`Longitude: ${lng} Latitude: ${lat} Zoom: ${zoom}`}</div>
+          <div className="Map-text">{`Number of Stations: ${this.state.stations.length}`}</div>
         </div>
-        {/* <div ref={el => this.mapContainer = el} className="absolute top right left bottom" /> */}
         <div className="Map-mapWrap" ref={el => this.mapContainer = el}/>
       </div>
     );
