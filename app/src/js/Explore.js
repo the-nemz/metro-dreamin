3735313c7b727b3c0d3ffda53ef3fd45713ab1cb
Isaac Nemzer <ijnemzer@outlook.com>
2021-03-03 01:57:25 -0500

    Added search button

--- app/src/js/Explore.js
+++ app/src/js/Explore.js
@@ -23,6 +23,11 @@ export function Explore(props) {
     window.addEventListener('resize', () => setWindowDims({ height: window.innerHeight, width: window.innerWidth }));
   }, []);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setQuery(input);
+  }
+
   const content = query ?
     <Search database={props.database} search={query} user={props.user} settings={props.settings} database={props.database} /> :
     <Discover database={props.database} user={props.user} settings={props.settings} database={props.database} />;
@@ -31,10 +36,15 @@ export function Explore(props) {
   return (
     <div className={exploreClass}>
       <div className="Explore-container">
-        <input className="Explore-input" value={input} placeholder={"Search for a map"}
-              onChange={(e) => setInput(e.target.value)}
-              onBlur={(e) => setQuery(e.target.value)}
-        />
+        <form className="Explore-inputWrap" onSubmit={handleSubmit}>
+          <input className="Explore-input" value={input} placeholder={"Search for a map"}
+                onChange={(e) => setInput(e.target.value)}
+                onBlur={(e) => query ? setQuery(e.target.value) : null}
+          />
+          <button className="Explore-searchButton" type="submit">
+            <i className="fas fa-search"></i>
+          </button>
+        </form>
         {content}
       </div>
     </div>
