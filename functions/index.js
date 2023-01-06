a0c6fc867b3477710cda0249084e0520505e5fd7
Isaac Nemzer <ijnemzer@outlook.com>
2023-01-06 00:47:13 -0800

    refactor firebase functions for db callbacks

--- functions/index.js
+++ functions/index.js
@@ -4,11 +4,13 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const cors = require('cors');
 const express = require('express');
-const mapboxStatic = require('@mapbox/mapbox-sdk/services/static');
 
-const { viewNotifications, addNotification } = require('./src/notifications.js');
-const { stars, getStarNotif } = require('./src/stars.js');
+const { viewNotifications } = require('./src/notifications.js');
+const { stars } = require('./src/stars.js');
 const { views } = require('./src/views.js');
+
+const { incrementStarsCount, decrementStarsCount } = require('./dbCallbacks/stars.js');
+const { generateSystemThumbnail } = require('./dbCallbacks/systems.js');
 
 const app = express();
 
@@ -51,133 +53,14 @@ app.put('/v1/views/:viewId', async (req, res) => await views(req, res));
 
 exports.api = functions.https.onRequest(app);
 
-
-
-
-
-
-
-
-const staticService = mapboxStatic({ accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA' });
-
 exports.incrementStarsCount = functions.firestore
   .document('systems/{systemId}/stars/{userId}')
-  .onCreate((snap, context) => {
-    const systemDoc = admin.firestore().doc(`systems/${context.params.systemId}`);
-    systemDoc.get().then((systemSnap) => {
-      if (systemSnap.exists) {
-        const systemData = systemSnap.data();
-
-        if (context.params.userId !== systemData.userId) {
-          const starrerDoc = admin.firestore().doc(`users/${context.params.userId}`);
-          starrerDoc.get().then((starrerSnap) => {
-            if (starrerSnap.exists) {
-              const starNotif = getStarNotif(starrerSnap.data(), systemData);
-              addNotification(systemData.userId, starNotif);
-            }
-          });
-        }
-
-        admin.firestore().doc(`systems/${context.params.systemId}`).update({
-          stars: (systemData.stars || 0) + 1
-        });
-      }
-    });
-  });
+  .onCreate(incrementStarsCount);
 
 exports.decrementStarsCount = functions.firestore
   .document('systems/{systemId}/stars/{userId}')
-  .onDelete((snap, context) => {
-    const systemDoc = admin.firestore().doc(`systems/${context.params.systemId}`);
-    systemDoc.get().then((systemSnap) => {
-      if (systemSnap.exists && systemSnap.data().stars) {
-        admin.firestore().doc(`systems/${context.params.systemId}`).update({
-          stars: systemSnap.data().stars - 1
-        });
-      }
-    });
-  });
+  .onDelete(decrementStarsCount);
 
 exports.generateSystemThumbnail = functions.firestore
   .document('systems/{systemId}')
-  .onWrite(async (systemChange, context) => {
-    if (!systemChange.after.exists) return;
-
-    const systemDocData = systemChange.after.data();
-
-    let lines = {};
-    const linesSnap = await admin.firestore().collection(`systems/${context.params.systemId}/lines`).get();
-    linesSnap.forEach((lineDoc) => {
-      const lineData = lineDoc.data();
-      lines[lineData.id] = lineData;
-    });
-
-    let stations = {};
-    const stationsSnap = await admin.firestore().collection(`systems/${context.params.systemId}/stations`).get();
-    stationsSnap.forEach((stationDoc) => {
-      const stationData = stationDoc.data();
-      stations[stationData.id] = stationData;
-    });
-
-    // taken from lib/util.js
-    const floatifyStationCoord = (station) => {
-      if (station == null) {
-        return station;
-      }
-
-      let { lng, lat } = station;
-      if (typeof lng === 'string') {
-        station.lng = parseFloat(lng)
-      }
-      if (typeof lat === 'string') {
-        station.lat = parseFloat(lat)
-      }
-      return station;
-    }
-
-    // taken from lib/util.js
-    const stationIdsToCoordinates = (stations, stationIds) => {
-      let coords = [];
-      for (const sId of stationIds) {
-        if (!stations[sId]) continue;
-        let { lng, lat } = floatifyStationCoord(stations[sId]);
-        coords.push([ lng, lat ]);
-      }
-      return coords;
-    }
-
-    let linePaths = [];
-    for (let i = 0; i < 30; i++) {
-      for (const lineKey in lines) {
-        const line = lines[lineKey];
-        const coords = stationIdsToCoordinates(stations, line.stationIds);
-
-        if (coords.length > 1) {
-          linePaths.push({
-            path: {
-              coordinates: coords,
-              strokeWidth: 4,
-              strokeColor: line.color,
-            }
-          })
-        }
-      }
-    }
-
-    staticService.getStaticImage({
-      ownerId: 'mapbox',
-      styleId: 'dark-v10',
-      attribution: false,
-      highRes: true,
-      width: 600,
-      height: 400,
-      position: 'auto',
-      overlays: linePaths
-    })
-      .send()
-      .then(response => {
-        const imageBuffer = Buffer.from(response.body, 'binary');
-        const thumbnailFile = admin.storage().bucket().file(context.params.systemId);
-        thumbnailFile.save(imageBuffer, { contentType: 'image/png' });
-      });
-  });
+  .onWrite(generateSystemThumbnail);
