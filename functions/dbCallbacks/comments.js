7e2521eac134cd384dcb2131c5aa481d2a2c51a1
Isaac Nemzer <ijnemzer@outlook.com>
2023-01-17 00:12:26 -0800

    create functions to handle comment creation and deletion: increment and decrement commentCount and send a notification

---
+++ functions/dbCallbacks/comments.js
@@ -0,0 +1,87 @@
+const admin = require('firebase-admin');
+
+const { addNotification } = require('../src/notifications.js');
+
+const incrementCommentsCount = (commentSnap, context) => {
+  const commentData = commentSnap.data();
+  const systemDoc = admin.firestore().doc(`systems/${context.params.systemId}`);
+  systemDoc.get().then((systemSnap) => {
+    if (systemSnap.exists) {
+      const systemData = systemSnap.data();
+
+      if (commentData.userId !== systemData.userId) {
+        const commenterDoc = admin.firestore().doc(`users/${commentData.userId}`);
+        commenterDoc.get().then((commenterSnap) => {
+          if (commenterSnap.exists) {
+            const commentNotif = getCommentNotif(commenterSnap.data(), systemData, commentData);
+            addNotification(systemData.userId, commentNotif);
+          }
+        });
+      }
+
+      admin.firestore().doc(`systems/${context.params.systemId}`).update({
+        commentsCount: (systemData.commentsCount || 0) + 1
+      });
+    }
+  });
+}
+
+const decrementCommentsCount = (snap, context) => {
+  const systemDoc = admin.firestore().doc(`systems/${context.params.systemId}`);
+  systemDoc.get().then((systemSnap) => {
+    if (systemSnap.exists && systemSnap.data().commentsCount) {
+      admin.firestore().doc(`systems/${context.params.systemId}`).update({
+        commentsCount: systemSnap.data().commentsCount - 1
+      });
+    }
+  });
+}
+
+function getCommentNotif(commenterData, systemData, commentData) {
+  const maxChars = 50;
+  const commentWords = (commentData.content || '').split(/\s/).filter(w => w.length > 0);
+
+  let commentPreview = '';
+  for (const [ind, word] of commentWords.entries()) {
+    if (ind === 0 && word.length > 50) {
+      commentPreview = word.substr(0, maxChars);
+    } else if (commentPreview.length + word.length + 1 < maxChars) {
+      commentPreview += `${ind !== 0 ? ' ' : ''}${word}`;
+    } else {
+      break;
+    }
+  }
+
+  if (commentPreview.split(/\s/).length < commentWords.length) {
+    commentPreview += '…';
+  }
+
+  return {
+    type: 'star',
+    destination: `/edit/${systemData.systemId}`,
+    image: 'star',
+    content: {
+      text: '[[starrerName]] just commented on your map [[mapTitle]]: "[[commentPreview]]"',
+      replacements: {
+        starrerName: {
+          text: commenterData.displayName ? commenterData.displayName : 'Anon',
+          styles: [
+            'italic'
+          ]
+        },
+        mapTitle: {
+          text: systemData.title ? systemData.title : 'Untitled',
+          styles: [
+            'bold',
+            'big'
+          ]
+        },
+        commentPreview: {
+          text: commentPreview
+        }
+      }
+    }
+  };
+}
+
+module.exports = { incrementCommentsCount, decrementCommentsCount };
