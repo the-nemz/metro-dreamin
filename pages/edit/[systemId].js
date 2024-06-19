6189536e101b2a8beaa5b0a73b071f470f967c66
Isaac Nemzer <ijnemzer@outlook.com>
2024-06-19 00:58:42 -0800

    use closer of two adjacent stations for distance in placement determination algo

--- pages/edit/[systemId].js
+++ pages/edit/[systemId].js
@@ -984,7 +984,7 @@ export default function Edit({
         firstStation = stations[line.stationIds[index - 1]];
         secondStation = station;
         thirdStation = stations[line.stationIds[index]];
-        distance = (getDistance(firstStation, secondStation) + getDistance(secondStation, thirdStation)) / 2;
+        distance = Math.min(getDistance(firstStation, secondStation), getDistance(secondStation, thirdStation));
       }
 
       // double check validity
@@ -995,16 +995,16 @@ export default function Edit({
       }
 
       // calculate rhumb bearings
-      const bearingDeg1 = turfRhumbBearing([ firstStation.lng, firstStation.lat ],
-                                           [ secondStation.lng, secondStation.lat ]);
-      const bearingDeg2 = turfRhumbBearing([ secondStation.lng, secondStation.lat ],
-                                           [ thirdStation.lng, thirdStation.lat ]);
+      const bearingDeg1 = turfRhumbBearing([ normalizeLongitude(firstStation.lng), firstStation.lat ],
+                                           [ normalizeLongitude(secondStation.lng), secondStation.lat ]);
+      const bearingDeg2 = turfRhumbBearing([ normalizeLongitude(secondStation.lng), secondStation.lat ],
+                                           [ normalizeLongitude(thirdStation.lng), thirdStation.lat ]);
 
       // find the rhumb distance to determine if it is on the other side of the world
-      const midPointOfOuter = turfMidpoint([ firstStation.lng, firstStation.lat ],
-                                           [ thirdStation.lng, thirdStation.lat ]);
+      const midPointOfOuter = turfMidpoint([ normalizeLongitude(firstStation.lng), firstStation.lat ],
+                                           [ normalizeLongitude(thirdStation.lng), thirdStation.lat ]);
       const rhumbDegrees = turfRhumbDistance(midPointOfOuter,
-                                             [ secondStation.lng, secondStation.lat ],
+                                             [ normalizeLongitude(secondStation.lng), secondStation.lat ],
                                              { units: 'degrees'});
 
       // find difference between bearings
