28dd55e2344e13f458e50b7ae2eb02a86d207836
alexander-harmaty <92049896+alexander-harmaty@users.noreply.github.com>
2024-07-24 21:13:18 -0400

    Sorting JSON

--- components/ImportAndExport.js
+++ components/ImportAndExport.js
@@ -1,7 +1,32 @@
 import React from 'react';
-import { FirebaseContext, getFullSystem, getUserDocData } from '/util/firebase.js';
+import { FirebaseContext } from '/util/firebase.js';
+import { getFullSystem } from '/util/firebase.js';
 import { doc, getDoc } from 'firebase/firestore';
-import { firestore } from '/util/firebase.js';
+
+function sortObjectByKey(obj) {
+  return Object.keys(obj)
+    .sort()
+    .reduce((result, key) => {
+      result[key] = obj[key];
+      return result;
+    }, {});
+}
+
+function sortStations(stations) {
+  const sortedStations = {};
+  Object.keys(stations).sort().forEach(stationId => {
+    const station = stations[stationId];
+    sortedStations[stationId] = {
+      id: station.id,
+      name: station.name,
+      lat: station.lat,
+      lng: station.lng,
+      ...(station.info && { info: sortObjectByKey(station.info) }),
+      ...(station.densityInfo && { densityInfo: sortObjectByKey(station.densityInfo) }),
+    };
+  });
+  return sortedStations;
+}
 
 export function ImportAndExport({ systemId, onSetToast }) {
   const firebaseContext = React.useContext(FirebaseContext);
@@ -9,26 +34,23 @@ export function ImportAndExport({ systemId, onSetToast }) {
   const handleExport = async () => {
     try {
       const fullSystem = await getFullSystem(systemId);
-      const systemDocRef = doc(firestore, `systems/${systemId}`);
-      const systemDoc = await getDoc(systemDocRef);
-      const systemData = systemDoc.data();
-
-      if (!systemData) {
-        throw new Error('System data not found');
-      }
-
-      const userDoc = await getUserDocData(systemData.userId);
-      const mapTitle = systemData.title || 'map';
-      const mapCreator = userDoc?.displayName || 'unknown_creator';
-
-      const formattedFileName = `${mapTitle}_by_${mapCreator}.json`;
-
-      const systemDataJSON = JSON.stringify(fullSystem, null, 2);
-      const blob = new Blob([systemDataJSON], { type: 'application/json' });
+      const sortedSystem = {
+        ...fullSystem,
+        map: {
+          ...fullSystem.map,
+          stations: sortStations(fullSystem.map.stations),
+        },
+      };
+      const systemData = JSON.stringify(sortedSystem, null, 2);
+      const systemDoc = await getDoc(doc(firebaseContext.database, `systems/${systemId}`));
+      const systemTitle = systemDoc.data().title || 'Untitled_Map';
+      const creatorDoc = await getDoc(doc(firebaseContext.database, `users/${systemDoc.data().userId}`));
+      const creatorName = creatorDoc.data().displayName || 'Unknown_Creator';
+      const blob = new Blob([systemData], { type: 'application/json' });
       const url = URL.createObjectURL(blob);
       const link = document.createElement('a');
       link.href = url;
-      link.download = formattedFileName;
+      link.download = `${systemTitle.replace(/\s+/g, '_')}_by_${creatorName.replace(/\s+/g, '_')}.json`;
       document.body.appendChild(link);
       link.click();
       document.body.removeChild(link);
@@ -43,7 +65,7 @@ export function ImportAndExport({ systemId, onSetToast }) {
   return (
     <div className="ImportAndExport">
       <button className="ImportAndExport-button" onClick={handleExport}>
-        <i className="fas fa-download"></i>
+        <i className="fas fa-download" style={{ color: 'orange' }}></i>
       </button>
       {/* Future Import functionality will be added here */}
     </div>
