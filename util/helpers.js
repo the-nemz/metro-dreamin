24e3e9a7ac3ce6b8a922f82a49ffd0df93054a41
David Aerne <meodai@gmail.com>
2024-10-07 01:07:00 +0200

    add 300ms debounce on name fetching to ease color name API calling

--- util/helpers.js
+++ util/helpers.js
@@ -1448,3 +1448,16 @@ export function renderSpinner(additionalClass) {
     </div>
   );
 }
+
+
+export function debounce(func, delay) {
+  let timeoutId;
+  return function (...args) {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+      func.apply(this, args);
+    }, delay);
+  };
+}
